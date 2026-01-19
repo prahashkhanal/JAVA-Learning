605f225d1d2d1fd44b2a3d0bf3607a2544face7c
prahashkhanal <prahashg99@gmail.com>
2026-01-19 10:13:54 +0530

    Added Instance variable

---
+++ Constructors/src/Instance.java
@@ -0,0 +1,17 @@
+class Students {
+    int id;
+    String name;
+
+    public static void main(String[] args) {
+        Students s1 = new Students();
+        s1.id = 101;
+        s1.name = "Prahash";
+        System.out.println(s1.id + " " + s1.name);
+
+        Students s2 = new Students();
+        s2.id = 102;
+        s2.name = "Pratyush";
+        System.out.println(s2.id + " " + s2.name);
+    }
+}
+
