b34a57251e31a4a31bfd3cc36470be5e09adf505
prahashkhanal <prahashg99@gmail.com>
2026-02-20 13:42:25 +0530

    16 feb monday assignments

---
+++ 16FebMonday/ExceptionDemo.java
@@ -0,0 +1,19 @@
+class ExceptionDemo {
+
+    static void fun() {
+        try {
+            throw new NullPointerException("demo");
+        } catch (NullPointerException e) {
+            System.out.println("Caught inside fun().");
+            throw e;   // rethrowing the exception
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            fun();
+        } catch (NullPointerException e) {
+            System.out.println("Caught in main.");
+        }
+    }
+}
